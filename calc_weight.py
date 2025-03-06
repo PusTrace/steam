c7cdadafff9a03e8f8c923171ba55783c89c1510
PusTrace <sergeivavilin2005@mail.ru>
2025-03-07 00:49:52 +0300

    weights

--- calc_weight.py
+++ calc_weight.py
@@ -1,6 +1,31 @@
 import json
-def calculate_weight_number_or_items(skin, lowest_price, median_price, volume, appearance_date, average_trend_percent, volatility_percent, approx_min, approx_max, order_info):
+import math
+from datetime import datetime
 
+def calculate_weight_number_of_items(lowest_price,
+                                    median_price,
+                                    order_info_87,
+                                    order_info_last,
+                                    volatility_percent,
+                                    appearance_date):
+    weight_of_orders = round(((order_info_last / order_info_87) * 100), 2)
+    
+    price = median_price if median_price is not None else lowest_price
+    # Коэффициент масштабирования для обратной экспоненты (можно подбирать эмпирически)
+    scale = 300.0
+    price_weight = math.exp(-price / scale) * 100
+    
+    current_year = datetime.now().year
+    # Предполагается, что appearance_date имеет формат "YYYY-MM-DD"
+    appearance_year = datetime.strptime(appearance_date, "%Y-%m-%d").year
+    # Чтобы избежать деления на ноль (в случае, если текущий год вдруг окажется 2013)
+    if current_year == 2013:
+        appearance_weight = 100
+    else:
+        appearance_weight = ((current_year - appearance_year) / (current_year - 2013)) * 100
+    
+    weight_number_of_items = weight_of_orders*0.5 + price_weight*0.5 + volatility_percent*0.5 + appearance_weight*0.5
+    return weight_number_of_items
 
 if __name__ == '__main__':
     with open("/home/pustrace/programming/trade/steam/database/perfect.json" , "r", encoding="utf-8") as f:
@@ -16,9 +41,19 @@ if __name__ == '__main__':
         approx_min = data["approx_min"]
         approx_max = data["approx_max"]
         order_info = data["order_info"]
+        order_info_87 = data["order_info_87"]
+        order_info_last = data["order_info_last"]
         
+        if order_info is None:
+            order_info = (order_info_87 * 3)
         if volume * 10 > order_info:
-        
+            weight_number_of_items = calculate_weight_number_of_items(skin,
+                                                                      lowest_price,
+                                                                      median_price,
+                                                                      order_info_87,
+                                                                      order_info_last,
+                                                                      volatility_percent,
+                                                                      appearance_date)
         
     
     
--- orders/sort_orders.py
+++ orders/sort_orders.py
@@ -8,7 +8,6 @@ with open("/home/pustrace/programming/trade/steam/database/perfect.json", 'r', e
 
 filtered_perfect_data = {}
 
-
 for skin, skin_info in perfect_data.items():
     try:
         median_price = skin_info["median_price"]
@@ -18,32 +17,30 @@ for skin, skin_info in perfect_data.items():
         print(f"Ошибка преобразования значений для {skin}: {e}")
         continue
 
-    # Вычисляем целевую цену: 75% от median_price (если есть) или от lowest_price
+    # Вычисляем целевую цену: 75% и 87% от median_price (если есть) или от lowest_price
     target_price = median_price * 0.75 if median_price is not None else lowest_price * 0.75
-    target_price_40 = median_price * 0.4 if median_price is not None else lowest_price * 0.4
     target_price_87 = median_price * 0.87 if median_price is not None else lowest_price * 0.87
+
     # Получаем данные по ордерам
     skin_orders = orders_data.get(skin, {})
     buy_order_graph = skin_orders.get("buy_order_graph", [])
 
     # Предполагается, что buy_order_graph отсортирован по цене в порядке убывания.
-    found_order = False
     for i in range(len(buy_order_graph) - 1):
         price_high = buy_order_graph[i][0]
         price_low = buy_order_graph[i+1][0]
         if price_high > target_price_87 > price_low:
-            # Проверяем, что ключа "order_info" ещё нет
             skin_info["order_info_87"] = buy_order_graph[i+1][1]
         if price_high > target_price > price_low:
-            # Проверяем, что ключа "order_info" ещё нет
             skin_info["order_info"] = buy_order_graph[i+1][1]
-        if price_high > target_price_40 > price_low:
-            # Проверяем, что ключа "order_info" ещё нет
-            skin_info["order_info_40"] = buy_order_graph[i+1][1]
+
+    # Если массив не пустой, сохраняем последний элемент массива
+    if buy_order_graph:
+        skin_info["order_info_last"] = buy_order_graph[-1][1]
 
     filtered_perfect_data[skin] = skin_info
 
-# Сохраняем обновлённые данные (например, в новый файл) с корректным выводом Unicode символов
+# Сохраняем обновлённые данные с корректным выводом Unicode символов
 with open("/home/pustrace/programming/trade/steam/database/perfect.json", "w", encoding="utf-8") as f:
     json.dump(filtered_perfect_data, f, indent=4, ensure_ascii=False)
 
